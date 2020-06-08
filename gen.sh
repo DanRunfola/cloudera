b86c49527a136dfa4473be7b339e62920dae1f7e
DanRunfola <dan@danrunfola.com>
2020-06-08 15:14:47 -0400

    h

--- gen.sh
+++ gen.sh
@@ -1,52 +1,35 @@
 #!/bin/bash
-
 # replace with applicationId to check logs:
 # yarn logs -applicationId application_1586456293454_0113
-
-
 # job variables
 # ==========
 # ==========
-
-env=geomesa
-py="3.7.7"
-run="/home/dsmillerrunfol@campus.wm.edu/cloudera/main.py"
-
+env=cloudera
+py="3.6.8"
+run="./main.py"
 # WM username (without the @CAMPUS.WM.EDU part)
 wmuser=dsmillerrunfol
-
 conda_dir=${HOME}
-
 # uncomment if using conda installed in working dir
 #conda_dir=$(pwd)
-
 # ==========
 # ==========
-
 wd=$(pwd)
-mkdir tests
-
-
 if [[ $(basename $(pwd)) != ${env} ]]; then
     echo "Current directory does not match Conda environment name"
     exit 1
 fi
-
 # instead of running the following init and source commands each time in
 # future, you could also add `source ${HOME}/.bashrc` to your
 # ${HOME}/.bash_profile file (bash_profile is run on login, bashrc is not)
-
 # init conda
 #${conda_dir}/anaconda3/bin/conda init
 # enable path to conda install set by init
 #source ${HOME}/.bashrc
-
 # make sure no env is active
 conda deactivate
-
 echo "----------------------------------------"
 echo "Building Conda Environment"
-
 # packages can be installed using conda, conda-forge, or pip
 #
 # *** unless you need to install using special options, use
@@ -57,95 +40,62 @@ echo "Building Conda Environment"
 #    - if using pip, do so after installing everything you
 #    need to with conda
 #    - pip info: https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-pkgs.html
-
-
 # create conda environment
 conda create -y -n ${env} --copy python=${py}
 conda activate ${env}
-
-
 # link CDH pyspark to env
 # ln -s /opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/lib/spark/python/pyspark ~/anaconda3/envs/${env}/lib/python3.6/site-packages/pyspark
-
-
-
 # make sure these are always installed
 conda install -y python-hdfs
-
 # conda requirements
 if [[ -f conda-requirements.txt ]];then
     conda install -y --file conda-requirements.txt
 else
     echo "No conda-requirements.txt file found"
 fi
-
 # conda custom install (uncomment to use)
 # ==========
 # conda install -y ...
 # ==========
-
 # conda-forge requirements
 if [[ -f conda-forge-requirements.txt ]];then
     conda install -y --channel conda-forge --file conda-forge-requirements.txt
 else
     echo "No conda-forge-requirements.txt file found"
 fi
-
 # conda-forge custom install (uncomment to use)
 # ==========
 # conda install -y --channel conda-forge ...
 # ==========
-
 # pip requirements
 if [[ -f pip-requirements.txt ]];then
-    pip3 install -r pip-requirements.txt
+    pip install -r pip-requirements.txt
 else
     echo "No pip-requirements.txt file found"
 fi
-
-
-
 # pip custom install (uncomment to use)
 # ==========
-#pip install --no-dependencies pytz 
-pip3 install /home/dsmillerrunfol@campus.wm.edu/cloudera/geomesa_pyspark-2.4.0.tar.gz
+# pip install ...
 # ==========
-
-
-
+pip3 install ${wd}/geomesa_pyspark-2.4.0.tar.gz
 echo "----------------------------------------"
 echo "Zipping Conda Environment"
-
 # zip conda environment
 cd ${conda_dir}/anaconda3/envs
 zip -r ${wd}/${env}.zip ${env}
 cd ${wd}
-
 conda deactivate
-
-
 echo "----------------------------------------"
-echo "Create main spark-submit file"
-
-# create spark submit file using conda environment
-cat << EOF > ss-${env}
-spark-submit \
---deploy-mode cluster \
---driver-memory=20g \
---conf spark.pyspark.driver.python=${conda_dir}/anaconda3/envs/${env}/bin/python \
---conf spark.pyspark.python=./ENV/${env}/bin/python \
---archives ${wd}/${env}.zip#ENV,/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/lib/spark/python/lib/py4j-0.10.7-src.zip \
---jars ${jar_path},/home/dsmillerrunfol@campus.wm.edu/cloudera/geomesa-accumulo-spark-runtime_2.11-2.4.0.jar,/home/dsmillerrunfol@campus.wm.edu/cloudera/geomesa-spark-converter_2.11-2.4.0.jar,/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/jars/httpclient-4.5.3.jar,/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/jars/commons-httpclient-3.1.jar \
-${run}
-EOF
-
+echo "Create pyspark file and main spark-submit file"
 # create pyspark file
 cat << EOF > ps-${env}
 pyspark \
---driver-memory=20g \
+--driver-memory=100g \
+--conf spark.rpc.message.maxSize=2047 \
+--conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+--conf spark.kryo.registrator=org.locationtech.rasterframes.util.RFKryoRegistrator \
+--conf spark.kryoserializer.buffer.max=500m \
 --conf spark.pyspark.driver.python=${conda_dir}/anaconda3/envs/${env}/bin/python \
 --conf spark.pyspark.python=./ENV/${env}/bin/python \
---archives ${wd}/${env}.zip#ENV,/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/lib/spark/python/lib/py4j-0.10.7-src.zip \
---jars ${jar_path},/home/dsmillerrunfol@campus.wm.edu/cloudera/geomesa-accumulo-spark-runtime_2.11-2.4.0.jar,/home/dsmillerrunfol@campus.wm.edu/cloudera/geomesa-spark-converter_2.11-2.4.0.jar,/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/jars/httpclient-4.5.3.jar,/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/jars/commons-httpclient-3.1.jar \
-EOF
-
+--archives ${wd}/${env}.zip#ENV,/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/lib/spark/python/lib/py4j-0.10.7-src.zip,${wd}/geomesa_pyspark-2.4.0.tar.gz \
+--jars ${wd}/geomesa-accumulo-spark-runtime_2.11-2.4.0.jar,${wd}/geomesa-spark-converter_2.11-2.4.0.jar,/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/jars/httpclient-4.5.3.jar,/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/jars/commons-httpclient-3.1.jar
